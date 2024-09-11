b01bad95d3bbb6196802e79dc36fcc7b94963013
t-godsey <t-godsey@onu.edu>
2024-09-11 12:13:32 -0400

    javascript for data transfer

--- static/scripts.js
+++ static/scripts.js
@@ -2,6 +2,8 @@
 // interativity code
 
 // Global Declarations
+let row_num = 1;
+
 
 // Main Code
 function reject() {
@@ -12,5 +14,25 @@ function accept() {
 
 }
 
+function data_transfer(){
+  fetch(`/read/${row_num}`)
+  .then(Response => Response.json())
+  .then(data => {
+    const obj = JSON.parse(data);
+    const ingredient_list = document.getElementById("ingredients-list");
+    ingredient_list.innerHTML = "";
+    document.getElementById("product-name").innerHTML = obj.product_name;
+    document.getElementById("product-type").innerHTML = "Product Type: " + obj.product_type;
+    document.getElementById("url").innerHTML = "Learn More: " + obj.product_url;
+    document.getElementById("price").innerHTML = "Price: " + obj.price;
+    var size = obj.clean_ingreds.length;
+    for(let i = 0; i < size; i++){
+      const listItem = document.createElement(li);
+      listItem.innerHTML = obj.clean_ingred[i];
+      ingredient_list.appendChild(listItem);
+    }
+    row_num++;
+  })
+}
 
 // Helper Functions
