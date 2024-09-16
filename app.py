5f95cf803769032c8758ec4ebb8db5283ea3228a
e-spinner <e-spinner@onu.edu>
2024-09-16 10:57:38 -0400

    ingreds fixed

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, jsonify
 from datetime import datetime
 import pandas as pd
+import ast
 
 # ====================== #
 # Global Declarations    #
@@ -19,16 +20,26 @@ app = Flask( __name__ )
 # home page
 @app.route( '/' )
 def index():
-    server_log( 'index', 'Rendering Main index.html')
+    server_log( 'index', 'Rendering Main index.html' )
     return render_template( 'index.html' )
 
 # data read in
 @app.route( '/read/<row>' )
 def read_data( row ):
+    
+    server_log( 'read-in', 'Translating ingreds' ) 
+    
+    ingreds =  df['clean_ingreds'][int(row)]
+    ingreds = ast.literal_eval(ingreds)
 
+    df['clean_ingreds'][int(row)] = ingreds[0:5]
+
+    
+    server_log( 'read-in', f'Reading row: {row}' ) 
     data = df.iloc[int(row), :].to_json()
-    server_log( 'read-in', f'Reading row: {row}' )
+       
     print(data)
+        
     return data
 
 # ====================== #
--- readme.md
+++ readme.md
@@ -3,9 +3,9 @@
 Base Functionality
 
 - [x] Base html structure
-- [ ] read_data fetch row from data set
-- [ ] dom start, accept(), and reject() js functions call for new data
-- [ ] some js to load data into html
+- [x] read_data fetch row from data set
+- [x] dom start, accept(), and reject() js functions call for new data
+- [x] some js to load data into html
 - [ ] css to make it all look less trash
 
 Extra Features
@@ -13,4 +13,4 @@ Extra Features
 - [ ] some method of seeing preiousy accepted products and clearing it
 - [ ] image of product from url, maybe url embed?
 - [ ] dark mode, or other customization options
-- [ ] other thing
+- [x] other thing
